a765dfeed3b1e28d41770150687f9d44dfe41ccd
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2025-04-17 11:53:33 +0300

    Create variables.tf

---
+++ project_vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "yc_token" {}
+variable "yc_cloud_id" {}
+variable "yc_folder_id" {}
+variable "zone" {
+  default = "ru-central1-a"
+}
+variable "ssh_public_key_path" {
+  description = "Путь до публичного SSH-ключа"
+  default     = "~/.ssh/id_rsa.pub"
+}
